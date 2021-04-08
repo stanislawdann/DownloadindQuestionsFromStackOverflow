12593d695470571614d2f8f2b1d534a56cbb3cc6
stanislawdann <dannstanislaw@gmail.com>
2021-04-08 22:04:10 +0200

    add params

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
 import json
+import pprint
+import webbrowser
 
-questions = {
-
+params = {
+    "site" : "stackoverflow",
+    "sort" : "votes",
+    "order" : "desc",
+    "fromdate" : "2021-04-01",
+    "tagged" : "python",
+    "min" : 6
 }
 
-r = requests.get("https://api.stackexchange.com/2.2/questions")
+r = requests.get("https://api.stackexchange.com/2.2/questions", params)
 
 try:
     questions = r.json()
 except json.decoder.JSONDecodeError:
     print("Error")
 else:
-    print(questions)
+    for question in questions["items"]:
+        pprint.pprint(question["link"])
